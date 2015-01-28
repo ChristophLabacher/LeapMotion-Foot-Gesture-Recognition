03b569455512c7f6b56dc697ebd95898fa28c6c0
Nikolas Klein <nikolas.klein@web.de>
2015-01-28 01:08:02 +0100

    some gesture adjustments

--- scripts/tracking/getGesture.js
+++ scripts/tracking/getGesture.js
@@ -29,7 +29,7 @@ function getSwipeVel()	{
 		// If the last four movements were left, right, left, none.
 		if (movementXHistory[movementXHistoryLength - 1].direction == "left" && movementXHistory[movementXHistoryLength - 1].velocity > swipeXVelocityBreakpoint && movementXHistory[movementXHistoryLength - 1].distance > swipeXDistanceBreakpoint
 		
-			&& movementXHistory[movementXHistoryLength - 1].startPosition < (width/2) + 100 && movementXHistory[movementXHistoryLength - 1].endPosition < width/2
+			&& movementXHistory[movementXHistoryLength - 1].startPosition < (width/2) + 220 && movementXHistory[movementXHistoryLength - 1].endPosition < width/2
 		)	{
 			
 			// Get the ids of those movements
@@ -50,7 +50,7 @@ function getSwipeVel()	{
 		// If the last three movements were right, left, none.
 		} else if (movementXHistory[movementXHistoryLength - 1].direction == "right" && movementXHistory[movementXHistoryLength - 1].velocity > swipeXVelocityBreakpoint && movementXHistory[movementXHistoryLength - 1].distance > swipeXDistanceBreakpoint
 		
-		&& movementXHistory[movementXHistoryLength - 1].startPosition > (width/2) - 100 && movementXHistory[movementXHistoryLength - 1].endPosition > width/2
+		&& movementXHistory[movementXHistoryLength - 1].startPosition > (width/2) - 220 && movementXHistory[movementXHistoryLength - 1].endPosition > width/2
 		
 		)	{
 			
